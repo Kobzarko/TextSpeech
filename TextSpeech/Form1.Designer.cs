519bfd9551aaa5fa19b563231d9f44fa65988798
Kobzarko <45173248+Kobzarko@users.noreply.github.com>
2019-06-29 14:25:18 +0300

    Добавьте файлы проекта.

---
+++ TextSpeech/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace TextSpeech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboSelectVoice = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnSpeak = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // cboSelectVoice
+            // 
+            this.cboSelectVoice.FormattingEnabled = true;
+            this.cboSelectVoice.Items.AddRange(new object[] {
+            "Не выбран",
+            "Мужской ",
+            "Женский",
+            "натуральный"});
+            this.cboSelectVoice.Location = new System.Drawing.Point(148, 27);
+            this.cboSelectVoice.Name = "cboSelectVoice";
+            this.cboSelectVoice.Size = new System.Drawing.Size(179, 24);
+            this.cboSelectVoice.TabIndex = 0;
+           
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выберете голос";
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(22, 91);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtContent.Size = new System.Drawing.Size(491, 353);
+            this.txtContent.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(418, 479);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(96, 37);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnResume.Location = new System.Drawing.Point(307, 479);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(105, 37);
+            this.btnResume.TabIndex = 3;
+            this.btnResume.Text = "Продолжить";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPause.Location = new System.Drawing.Point(221, 479);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(80, 37);
+            this.btnPause.TabIndex = 3;
+            this.btnPause.Text = "Пауза";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSpeak.Location = new System.Drawing.Point(128, 479);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(87, 37);
+            this.btnSpeak.TabIndex = 3;
+            this.btnSpeak.Text = "Говорить";
+            this.btnSpeak.UseVisualStyleBackColor = true;
+            this.btnSpeak.Click += new System.EventHandler(this.btnSpeak_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(371, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 58);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Распознать текст";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 528);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSpeak);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboSelectVoice);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TextSpeech";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboSelectVoice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnResume;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnSpeak;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ TextSpeech/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.IO;
+using Tesseract;
+
+namespace TextSpeech
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer voice;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            voice = new SpeechSynthesizer();
+        }
+
+        private void btnSpeak_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                switch (cboSelectVoice.SelectedIndex)
+                {
+                    case 0:
+                        voice.SelectVoiceByHints(VoiceGender.NotSet);
+                        break;
+                    case 1:
+                        voice.SelectVoiceByHints(VoiceGender.Male);
+                        break;
+                    case 2:
+                        voice.SelectVoiceByHints(VoiceGender.Female);
+                        break;
+                    case 3:
+                        voice.SelectVoiceByHints(VoiceGender.Neutral);
+                        break;
+
+                    default:
+                        break;
+                }
+                voice.SpeakAsync(txtContent.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                voice.Pause();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                voice.Resume();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SaveFileDialog sfd = new SaveFileDialog())
+                {
+                    sfd.Filter = "Wave files|*.wav";
+                    sfd.Title = "Save to a wave file";
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+                        FileStream fs = new FileStream(sfd.FileName, FileMode.Create, FileAccess.Write);
+                        voice.SetOutputToWaveStream(fs);
+                        voice.Speak(txtContent.Text);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+     
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var img = new Bitmap(openFileDialog.FileName);
+                    var ocr = new TesseractEngine("./tessdata", "eng", EngineMode.TesseractAndCube);
+                    var page = ocr.Process(img); // распознавание recognition
+
+                    txtContent.Text = page.GetText(); // вот это надо добавить в string NumberID 
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+
+            }
+        }
+    }
+}
